82cb290f813864bed9f377f4797589f6859037c5
James Socol <james@mozilla.com>
2010-08-13 20:40:58 -0400

    Auth URLs. No templates yet.

--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', include('doozer.urls')),
+    (r'^accounts/', include('auth_urls')),
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
