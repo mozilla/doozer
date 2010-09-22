34e14fbdfd8edd2538cd9d69cd29d784eab55931
James Socol <james@mozilla.com>
2010-09-21 20:10:56 -0400

    Games templates cleanup and games/mine view.

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, url, include
 urlpatterns = patterns('games.views',
     url(r'^$', 'view_list', name='games.view_list'),
     url(r'^(?P<game_id>\d+)/(?P<slug>[\w-]+)?', 'view', name='games.view'),
+    url(r'^mine$', 'mine', name='games.mine'),
     url(r'^create', 'create', name='games.create'),
     url(r'^edit/(?P<game_id>\d+)$', 'edit', name='games.edit'),
     url(r'^delete/(?P<game_id>\d+)$', 'delete', name='games.delete'),
