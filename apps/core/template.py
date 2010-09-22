2c08001f0e1ab88e4a7e651188eede6114e4b1be
Ricky Rosario <rickyrosario@gmail.com>
2010-09-22 12:20:33 -0400

    Fixed the templates for 403, 404, and 500

--- apps/core/template.py
+++ apps/core/template.py
@@ -1,9 +1,11 @@
 from django.core.context_processors import csrf
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.http import HttpResponse
+from django.template import RequestContext, Template
+from django.template.loader import get_template
 
 
-def render(request, template, data=None, mimetype=None):
+def render(request, template, data=None, mimetype=None, status=200):
     """Render a template with a RequestContext."""
-    c = RequestContext(request)
-    return render_to_response(template, data, c, mimetype=mimetype)
+    t = get_template(template)
+    c = RequestContext(request, data)
+    return HttpResponse(t.render(c), mimetype=mimetype, status=status)
